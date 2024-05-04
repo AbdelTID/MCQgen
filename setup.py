da7055878a05a094e297d63aca0d88d285636541
AbdelTID <abdelwizanlah@gmail.com>
2024-05-04 23:58:13 +0100

    genmcq final-01

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     author='abdelanlah',
     author_email='abdelwizanlah@gmail.com',
-    install_requires=["langchain","streamlit","python-dotenv","PyPDF2","huggingface_hub"],
+    install_requires=["langchain","streamlit","python-dotenv","PyPDF2","huggingface_hub","google-search-results"],
     packages=find_packages()
 )
 
