a8a00fdeec08c64a792b3a541b31a0f4f0d12fa7
AbdelTID <abdelwizanlah@gmail.com>
2024-05-04 22:04:27 +0100

    genmcq updated

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='abdelanlah',
+    author_email='abdelwizanlah@gmail.com',
+    install_requires=["langchain","streamlit","python-dotenv","PyPDF2","huggingface_hub"],
+    packages=find_packages()
+)
+
+
+
